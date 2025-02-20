ac7aead6fa416b5ded7bc97f1f93f213dd90809a
amnarehmani <jeonamnaaa@gmail.com>
2025-02-20 22:14:26 +0500

    learning python:

---
+++ lecture 01/helloWorld.py
@@ -0,0 +1,109 @@
+print("hello world");
+print ("my name is Amna", "i'm 18 years old")
+print(18)
+print (23 + 8)
+print(4 * 5)
+
+
+# variables
+
+name = "Amna";
+age = 18
+birthday = 20
+
+print (name , age , birthday)
+
+print ("my name is:" , name) 
+print ("my age is: " , age )
+
+# data types
+
+print(type(name))
+print(type(age))
+print(type(birthday))
+
+name1 = 'amna';
+name2 = "amna"
+name3 = '''amna'''
+
+print(name1)
+print(name2)
+print(name3)
+
+boolean = True
+none = None
+numberFloat = 0.54
+number = 44
+
+print(boolean)
+print(none)
+print(numberFloat)
+print(number)
+
+print(type(boolean))
+print(type(none))
+print(type(numberFloat))
+print(type(number))
+
+
+# printing sums
+
+num1 = 8
+num2 = 6
+diff = num1 + num2
+print(diff)
+print(num1 + num2 )
+print(num1 -num2)
+print(num1 % num2)
+print(num1 / num2)
+print(num1 * num2)
+
+# operators
+
+
+# arithmetic operator
+a = 5
+b = 4
+print(a + b )
+print(a - b)
+print(a % b)
+print(a / b)
+print(a * b) #remainder
+print(a ** b ) #a^b 
+
+
+# relational operator
+
+a = 50
+b = 60
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+
+# assignment operator
+a = 9
+# a = a + 44 
+a+= 44
+print(a)
+
+# logical operator
+print(not False)
+print(not True)
+print(not(a > b))
+
+value1 = True
+value2 = False
+print("value AND:",value1 and value2)
+print("value OR:",value1 or value2)
+print("values are:", (a == b) and (a != b))
+print("values are:", (a == b) or (a != b))
+
+
+
+# type conversion
