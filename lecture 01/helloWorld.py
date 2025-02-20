ba5d044eb7d17a5d0dfcccb6d743e086e99dbf24
amnarehmani <jeonamnaaa@gmail.com>
2025-02-20 23:51:01 +0500

    learning python

--- lecture 01/helloWorld.py
+++ lecture 01/helloWorld.py
@@ -107,3 +107,92 @@ print("values are:", (a == b) or (a != b))
 
 
 # type conversion
+a = float(5)
+d = str(5)
+c = int("5")
+b = 5.25
+sum = a + b
+print(type(a))
+print(type(d))
+print(type(c))
+
+print(sum)
+
+
+num = 3.66
+num = str(3.66)
+print(type(num))
+
+# inputs
+
+input("enter your name: ")
+
+name = input("enter your name: ")
+print("welcome" , name)
+
+# always return str
+val = input("enter value")
+print("enterd value:" ,  type(val) , val)
+
+# if want another type 
+
+numInput = int(input("enter num"));
+print("entered num" , type(numInput) , numInput)
+
+FloatInput = float(input("enter num"));
+print("entered num" , type(FloatInput) , FloatInput)
+
+
+# small exercise
+
+name = str(input("enter name"))
+age = int(input("enter age"))
+marks = float(input("enter marks"))
+
+print(type(name) , "welcome" "your name is: " , name)
+print(type(age) , "your age is: " , age)
+print(type(marks) , "your marks are: " , marks)
+
+
+# practice questions :
+# write a program to input 2 numbers & print their sums
+
+input1 = input("enter number one")
+input2 = input("enter number two")
+
+print("result:" , input1 + input2)
+
+input1 = int(input("enter number one"))
+input2 = int(input("enter number two"))
+
+print("result:" , input1 - input2)
+print("result:" , input1 / input2)
+print("result:" , input1 * input2)
+
+
+# write a program to input a side of square & print its area
+
+
+side = float(input("enter square side"))
+
+print("area of sqaure is:" , side * side)
+
+
+
+# write a program of 2 floating numbers and print their average
+
+x= float(input("enter number one"))
+y = float(input("enter number two"))
+
+print("answer: " , (x+y)/2 )
+
+
+# write a program of 2 int numbers a and b 
+# print true if eqaul than or greater than b , if not print false
+
+
+
+
+int1 = int(input("enter number one"))
+int2 = int(input("enter number two"))
+print(a>=b)
