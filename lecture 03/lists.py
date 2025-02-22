1ab2ac0648c93b34b315b6715bdbc6fb2157bea5
amnarehmani <jeonamnaaa@gmail.com>
2025-02-22 22:10:59 +0500

    learning python

---
+++ lecture 03/lists.py
@@ -0,0 +1,14 @@
+marks = [33, 55 , 89.7 , 67.8 , 78.1]
+print(marks)
+print(type(marks))
+print(marks[3])
+
+student = [ "amna" ,  18 , "karachi"]
+print(student)
+print("name" , student[0])
+print("age" , student[1])
+print("city" , student[2])
+
+student[0] = "aiza"
+print("name" , student[0])
+print(student)
